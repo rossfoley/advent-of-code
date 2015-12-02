693f2c0491117f0f0cba1a60cb81748247aa0175
Ross Foley <ross3141@gmail.com>
2015-12-01 23:48:36 -0800

    Day 2 Part 2 Solution

--- day2.rb
+++ day2.rb
@@ -1,3 +1,4 @@
+# Part 1: Total Wrapping Paper
 total = File.read('day2-input.txt').lines.map do |line|
   dimensions = line.split('x').map(&:to_i)
   areas = [2 * dimensions[0] * dimensions[1],
@@ -5,4 +6,17 @@ total = File.read('day2-input.txt').lines.map do |line|
            2 * dimensions[1] * dimensions[2]]
   areas.reduce(:+) + (areas.min / 2)
 end.reduce(:+)
+
 puts "Total Wrapping Paper: #{total} square feet"
+
+
+# Part 2: Total Ribbon
+total = File.read('day2-input.txt').lines.map do |line|
+  dimensions = line.split('x').map(&:to_i).sort
+  ribbon_wrap = 2 * (dimensions[0] + dimensions[1])
+  ribbon_bow = dimensions.reduce(:*)
+  ribbon_wrap + ribbon_bow
+end.reduce(:+)
+
+puts "Total Ribbon: #{total} feet"
+
