e1edf1835fa521a12e1ef1d8d3a8d76c23d96179
Ross Foley <ross3141@gmail.com>
2016-12-06 00:16:08 -0800

    Day 5 Part 2 2016

--- 2016/day05/code.go
+++ 2016/day05/code.go
@@ -1,6 +1,5 @@
 package main
 
-// import s "strings"
 import (
 	"crypto/md5"
 	"encoding/hex"
@@ -15,7 +14,19 @@ func GetMD5Hash(text string) string {
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
-func main() {
+func ReplaceAtIndex(str string, sub string, index int64) string {
+	return str[:index] + sub + str[index+1:]
+}
+
+func ParseInt(str string) int64 {
+	i, err := strconv.ParseInt(str, 10, 32)
+	if err != nil {
+		return -1
+	}
+	return i
+}
+
+func SolvePart1() {
 	PUZZLE_INPUT := "reyedfim"
 	index := 0
 	password := ""
@@ -30,5 +41,34 @@ func main() {
 		index += 1
 	}
 
-	fmt.Println("Final Password: " + password)
+	fmt.Println("Part 1 Password: " + password)
+}
+
+func SolvePart2() {
+	PUZZLE_INPUT := "reyedfim"
+	index := 0
+	password := "zzzzzzzz"
+
+	for strings.Contains(password, "z") {
+		hash := GetMD5Hash(PUZZLE_INPUT + strconv.Itoa(index))
+
+		if strings.HasPrefix(hash, "00000") {
+			position, char := ParseInt(hash[5:6]), hash[6:7]
+
+			if position > -1 && position < 8 {
+				if password[position:position+1] == "z" {
+					password = ReplaceAtIndex(password, char, position)
+				}
+			}
+		}
+
+		index += 1
+	}
+
+	fmt.Println("Part 2 Password: " + password)
+}
+
+func main() {
+	SolvePart1()
+	SolvePart2()
 }
