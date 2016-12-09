5a6f000ca7c2243c1041cc3d6644b5dae678ac46
Ross Foley <ross3141@gmail.com>
2016-12-09 08:09:53 -0800

    Day 9 2016

--- 2016/day09/part2.rb
+++ 2016/day09/part2.rb
@@ -1,42 +1,22 @@
-class DecompressorSection
-  def initialize input, multiplier
+class Decompressor
+  def initialize input
     @input = input
-    @multiplier = multiplier
-    @unexpanded_count = 0
-    @children = []
   end
 
-  # Convert input string into hierarchy of child sections
-  def process
-    remaining = @input
-    while remaining.length > 0
-      match = remaining.match /(.*?)\((\d+)x(\d+)\)(.+)/
-      if match.nil?
-        @unexpanded_count = remaining.length
-        remaining = ''
-      else
-        before, rest = match[1], match[4]
-        length, times = match[2].to_i, match[3].to_i
-        next_marker = rest.index('(', length) || rest.length
-
-        child_section = DecompressorSection.new(rest[0...length], times)
-        child_section.process
-        @children << child_section
-
-        @unexpanded_count += next_marker - length + before.length
-        remaining = rest[next_marker..-1]
-      end
-    end
+  def final_length
+    decompressed_length @input
   end
 
-  def decompressed_length
-    children_length = @children.map {|c| c.decompressed_length}.reduce(:+) || 0
-    (@multiplier * children_length) + @unexpanded_count
+  def decompressed_length input
+    match = input.match /(.*?)\((\d+)x(\d+)\)(.+)/
+    return input.length if match.nil?
+
+    before, length, times, rest = match[1], match[2].to_i, match[3].to_i, match[4]
+    before.length + (times * decompressed_length(rest[0...length])) + decompressed_length(rest[length..-1])
   end
 end
 
 input = File.read('input.txt').lines[0].strip
-decompressor = DecompressorSection.new(input, 1)
-decompressor.process
+decompressor = Decompressor.new input
 
-puts "Part 2: #{decompressor.decompressed_length}"
+puts "Part 2: #{decompressor.final_length}"
