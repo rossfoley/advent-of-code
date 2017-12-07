24a1dc60ba6c5bbc8ad28aac886961146968013f
Ross Foley <ross3141@gmail.com>
2017-12-07 00:32:42 -0800

    Day 6 2017

--- 2017/day06/code.rb
+++ 2017/day06/code.rb
@@ -1,44 +1,36 @@
-class Day05
-  def part1 input
-    jumps = input.map(&:strip).map(&:to_i)
-    pc = 0
-    steps = 0
+class Day06
+  def solve input
+    banks = input.strip.split("\t").map(&:to_i)
+    seen = []
+    latest = ""
+    cycles = 0
 
-    while pc < jumps.size
-      jump = jumps[pc]
-      jumps[pc] += 1
-      pc += jump
-      steps += 1
-    end
-    
-    steps
-  end
+    while !(seen.include? latest)
+      seen << latest
 
-  def part2 input
-    jumps = input.map(&:strip).map(&:to_i)
-    pc = 0
-    steps = 0
+      # Find largest bank
+      max = banks.max
+      max_index = banks.index max
 
-    while pc < jumps.size
-      jump = jumps[pc]
-      if jump >= 3
-        jumps[pc] -= 1
-      else
-        jumps[pc] += 1
+      # Redistribute that bank's value
+      banks[max_index] = 0
+      (1..max).each do |i|
+        banks[(max_index + i) % banks.size] += 1
       end
-      pc += jump
-      steps += 1
+
+      # Store the latest configuration
+      latest = banks.join ''
+      cycles += 1
     end
-    
-    steps
+
+    loop_length = cycles - seen.index(latest)
+    [cycles, loop_length]
   end
 end
 
-day5 = Day05.new
-input = File.read('input.txt').lines
+day6 = Day06.new
+input = File.read('input.txt').lines.first
 
-part1 = day5.part1(input)
+part1, part2 = day6.solve(input)
 puts "Part 1: #{part1}"
-
-part2 = day5.part2(input)
 puts "Part 2: #{part2}"
