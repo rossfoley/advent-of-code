0c469e89a86bf3f9f07e8fccacfb865ff91910fa
Ross Foley <ross3141@gmail.com>
2017-12-24 23:52:19 -0800

    Day 25 2017

--- 2017/day25/code.rb
+++ 2017/day25/code.rb
@@ -1,5 +1,75 @@
+class State
+  attr_accessor :name, :rules
+
+  def initialize name, rules
+    @name = name
+    @rules = rules
+  end
+end
+
+class TuringMachine
+  attr_accessor :state, :states, :tape, :position
+
+  def initialize start, states
+    @state = start
+    @states = states
+    @tape = Array.new(1_000_000) { 0 }
+    @position = 500_000
+  end
+
+  def tick
+    value = @tape[@position]
+    rule = @states[@state].rules[value]
+
+    @tape[@position] = rule[:write]
+    @position += rule[:move]
+    @state = rule[:new_state]
+  end
+end
+
 class Day25
+  def parse_input input
+    input.reject! {|a| a.empty?} # Remove newlines
+    input.map! {|a| a[0...-1]} # Strip all periods
+
+    # Get the header data
+    start = input.first.split(' ').last
+    diagnostic = input[1].split(' ')[-2].to_i
+    states = {}
+
+    # All necessary data occurs as the last word on each line
+    input.map! {|a| a.split(' ').last}
+
+    # Start after the header
+    line = 2
+    while line < input.size
+      name = input[line]
+      rules = {
+        0 => {
+          write: input[line + 2].to_i,
+          move: input[line + 3] == 'left' ? -1 : 1,
+          new_state: input[line + 4]
+        },
+        1 => {
+          write: input[line + 6].to_i,
+          move: input[line + 7] == 'left' ? -1 : 1,
+          new_state: input[line + 8]
+        }
+      }
+      states[name] = State.new name, rules
+      line += 9
+    end
+
+    [start, diagnostic, states]
+  end
+
   def part1 input
+    start, diagnostic, states = parse_input input
+    turing = TuringMachine.new start, states
+
+    diagnostic.times { turing.tick }
+
+    turing.tape.reduce(:+)
   end
 
   def part2 input
